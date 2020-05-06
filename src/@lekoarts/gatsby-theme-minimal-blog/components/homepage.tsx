1dbd4d48bc2bd9140669fad72c7d09b373179403
Kentaroh Awata <kentarohawata@ReproAdminnoMacBook-Pro.local>
2020-05-06 15:08:10 +0900

    minor fix

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -2,7 +2,7 @@
 import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 import Layout from "./layout"
-import Title from "./titl
+import Title from "./title
 import Listing from "./listing"
 import List from "./list"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
